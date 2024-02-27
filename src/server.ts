408c57d4bc8f2e0d9b921c71071dfc2b002225e2
Yannic Brügger <yannic.bruegger@gmail.com>
2024-02-27 12:29:51 +0000

    chore: lint code

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-import { hostname, port } from './environment.ts'
-import { validateRequest } from './request-validation.ts'
+import { hostname, port } from './environment.ts';
+import { validateRequest } from './request-validation.ts';
 
 const handler = async (req: Request) => {
   // Perform validation
-  const validationResult = await validateRequest(req)
-  if (!validationResult.isValid) return new Response(validationResult.message, { status: validationResult.statusCode })
+  const validationResult = await validateRequest(req);
+  if (!validationResult.isValid) {
+    return new Response(validationResult.message, {
+      status: validationResult.statusCode,
+    });
+  }
 
-  // TODO(@yannic-bruegger): Perform request 
+  // TODO(@yannic-bruegger): Perform request
 
   // Send response
   return new Response(`Successfully updated ${new URL(req.url).pathname}!`);
-}
+};
 
 Deno.serve({ port, hostname, handler });
