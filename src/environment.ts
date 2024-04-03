7edfbf9b3ec9b244d5a8596cfc27bea3ddfc98b6
Yannic Brügger <yannic.bruegger@gmail.com>
2024-04-03 17:06:41 +0000

    fix duplicate hostname variable

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ import { load } from 'https://deno.land/std@0.216.0/dotenv/mod.ts';
 
 const ENV = await load();
 export const port = parseInt(ENV['PORT']);
-export const hostname = ENV['HOSTNAME'];
+export const hostname = ENV['HOST_NAME'];
 export const imageBasePath = ENV['IMAGE_BASE_PATH'];
 export const jsonDataFileSuffix = ENV['JSON_DATA_FILE_SUFFIX'];
 export const apiKey = ENV['API_KEY'];
